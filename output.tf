13a8fe1f402a4c1f6dc4ade6caace24f1de6637a
mohammedrahamadulla <rahamadulla786.md@gmail.com>
2022-12-26 21:11:41 +0530

    Added Datasource for docker_resource

--- output.tf
+++ output.tf
@@ -1,24 +1,24 @@
-output vpc_id {
-  value       = aws_vpc.testvpc01.id
-  sensitive   = false
+output "vpc_id" {
+  value     = aws_vpc.testvpc01.id
+  sensitive = false
 }
 
-output vpc_arn {
-  value       = aws_vpc.testvpc01.arn
-  sensitive   = false
-} 
+output "vpc_arn" {
+  value     = aws_vpc.testvpc01.arn
+  sensitive = false
+}
 
-output subnet_1 {
-  value       = aws_subnet.subnet1-public.id
-  sensitive   = false
-} 
+output "subnet_1" {
+  value     = aws_subnet.subnet1-public.id
+  sensitive = false
+}
 
-output subnet_2 {
-  value       = aws_subnet.subnet2-public.id
-  sensitive   = false
-} 
+output "subnet_2" {
+  value     = aws_subnet.subnet2-public.id
+  sensitive = false
+}
 
-output subnet_3 {
-  value       = aws_subnet.subnet2-public.id
-  sensitive   = false
+output "subnet_3" {
+  value     = aws_subnet.subnet2-public.id
+  sensitive = false
 } 
